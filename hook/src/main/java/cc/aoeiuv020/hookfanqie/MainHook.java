d9b19d12f5fa832c8d23e67fba0539b0ca819071
root <root@localhost>
2023-07-10 02:35:45 +0800

    -去除更新弹窗 -去除听书页面热门直播 -更换VIP的Hook点方便适配后续版本

--- hook/src/main/java/cc/aoeiuv020/hookfanqie/MainHook.java
+++ hook/src/main/java/cc/aoeiuv020/hookfanqie/MainHook.java
@@ -8,6 +8,7 @@ import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
+import java.lang.reflect.Method;
 
 @SuppressWarnings("RedundantThrows")
 public class MainHook implements IXposedHookLoadPackage {
@@ -24,6 +25,8 @@ public class MainHook implements IXposedHookLoadPackage {
                 hookDebug(lpparam);
                 hookVip(lpparam);
                 hookKillAd(lpparam);
+                hookUpdate(lpparam);
+                hookPoplive(lpparam);
                 hookLuckyDog(lpparam);
             }
         });
@@ -39,6 +42,25 @@ public class MainHook implements IXposedHookLoadPackage {
         });
     }
 
+    private void hookPoplive(XC_LoadPackage.LoadPackageParam lpparam) {
+        try {
+            Class<?> clazz = XposedHelpers.findClass("com.dragon.read.widget.m", lpparam.classLoader);
+            Method[] methods = XposedHelpers.findMethodsByExactParameters(clazz, null);
+            for (Method method : methods) {
+                if (method.getName().equals("a")) {
+                    XposedBridge.hookMethod(method, new XC_MethodReplacement() {
+                        @Override
+                        protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+                            return null;
+                        }
+                    });
+                }
+            }
+        } catch (Throwable t) {
+            XposedBridge.log(t);
+        }
+    }
+
     private void hookKillAd(XC_LoadPackage.LoadPackageParam lpparam) {
         XposedHelpers.findAndHookMethod("com.dragon.read.user.h", lpparam.classLoader, "e", new XC_MethodHook() {
             @Override
@@ -47,15 +69,37 @@ public class MainHook implements IXposedHookLoadPackage {
             }
         });
     }
-
-    private void hookVip(XC_LoadPackage.LoadPackageParam lpparam) {
-        XposedHelpers.findAndHookMethod("com.dragon.read.user.h", lpparam.classLoader, "a", new XC_MethodHook() {
+    
+    private void hookUpdate(XC_LoadPackage.LoadPackageParam lpparam) {
+        XposedHelpers.findAndHookMethod("com.ss.android.update.ad", lpparam.classLoader, "k", new XC_MethodHook() {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                param.setResult(true);
+                param.setResult(false);
             }
         });
     }
+
+    private void hookVip(XC_LoadPackage.LoadPackageParam lpparam) {
+        XposedHelpers.findAndHookConstructor("com.dragon.read.user.model.VipInfoModel", lpparam.classLoader,
+            String.class, String.class, String.class, boolean.class, boolean.class, int.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                Object[] args = param.args;
+                String expireTime = (String) args[0];
+                String isVip = (String) args[1];
+                String leftTime = (String) args[2];
+                boolean isAutoCharge = (boolean) args[3];
+                boolean isUnionVip = (boolean) args[4];
+                String union_source = (int) args[5];
+                args[0] = "4102415999";
+                args[1] = "1";
+                args[2] = "10000";
+                args[3] = true;
+                args[4] = true;
+                args[5] = 1;
+            }
+        });
+     }
 
     private void log(XC_MethodHook.MethodHookParam param) {
         XposedBridge.log("hook: " + param.thisObject.getClass().getName() + "." + param.method.getName());
