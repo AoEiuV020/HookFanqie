4120bb6c832b8b60b4e5ffc9f9ad6bdf3645af09
AoEiuV020 <aoeiuv020@gmail.com>
2023-07-11 01:48:06 +0800

    缩进格式化

--- hook/src/main/java/cc/aoeiuv020/hookfanqie/MainHook.java
+++ hook/src/main/java/cc/aoeiuv020/hookfanqie/MainHook.java
@@ -44,12 +44,12 @@ public class MainHook implements IXposedHookLoadPackage {
 
     private void hookPoplive(XC_LoadPackage.LoadPackageParam lpparam) {
         XposedHelpers.findAndHookMethod("com.dragon.read.component.audio.impl.ui.b.a", lpparam.classLoader, "a",
-            Context.class, String.class, String.class, new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                param.setResult(null);
-            }
-        });
+                Context.class, String.class, String.class, new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        param.setResult(null);
+                    }
+                });
     }
 
     private void hookKillAd(XC_LoadPackage.LoadPackageParam lpparam) {
@@ -60,7 +60,7 @@ public class MainHook implements IXposedHookLoadPackage {
             }
         });
     }
-    
+
     private void hookUpdate(XC_LoadPackage.LoadPackageParam lpparam) {
         XposedHelpers.findAndHookMethod("com.ss.android.update.ad", lpparam.classLoader, "k", new XC_MethodHook() {
             @Override
@@ -72,25 +72,25 @@ public class MainHook implements IXposedHookLoadPackage {
 
     private void hookVip(XC_LoadPackage.LoadPackageParam lpparam) {
         XposedHelpers.findAndHookConstructor("com.dragon.read.user.model.VipInfoModel", lpparam.classLoader,
-            String.class, String.class, String.class, boolean.class, boolean.class, int.class, new XC_MethodHook() {
-            @Override
-            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                Object[] args = param.args;
-                String expireTime = (String) args[0];
-                String isVip = (String) args[1];
-                String leftTime = (String) args[2];
-                boolean isAutoCharge = (boolean) args[3];
-                boolean isUnionVip = (boolean) args[4];
-                int union_source = (int) args[5];
-                args[0] = "4102415999";
-                args[1] = "1";
-                args[2] = "10000";
-                args[3] = true;
-                args[4] = true;
-                args[5] = 1;
-            }
-        });
-     }
+                String.class, String.class, String.class, boolean.class, boolean.class, int.class, new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        Object[] args = param.args;
+                        String expireTime = (String) args[0];
+                        String isVip = (String) args[1];
+                        String leftTime = (String) args[2];
+                        boolean isAutoCharge = (boolean) args[3];
+                        boolean isUnionVip = (boolean) args[4];
+                        int union_source = (int) args[5];
+                        args[0] = "4102415999";
+                        args[1] = "1";
+                        args[2] = "10000";
+                        args[3] = true;
+                        args[4] = true;
+                        args[5] = 1;
+                    }
+                });
+    }
 
     private void log(XC_MethodHook.MethodHookParam param) {
         XposedBridge.log("hook: " + param.thisObject.getClass().getName() + "." + param.method.getName());
