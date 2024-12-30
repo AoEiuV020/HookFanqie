55276d600bdc403852eba4a3ee81ecf79568d98b
AoEiuV020 <aoeiuv020@gmail.com>
2024-12-31 01:24:22 +0800

    修hook模块报红问题

--- hook/build.gradle.kts
+++ hook/build.gradle.kts
@@ -74,7 +74,6 @@ android {
 }
 dependencies {
     compileOnly("de.robv.android.xposed:api:82")
-    compileOnly("de.robv.android.xposed:api:82:sources")
 }
 
 apply(rootDir.resolve("gradle/signing.gradle"))
