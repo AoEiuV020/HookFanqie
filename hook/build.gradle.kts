9a9b6c7d96acb49e6aecb81bf7f95e04f61c54cd
AoEiuV020 <aoeiuv020@gmail.com>
2024-12-31 01:28:42 +0800

    禁混淆
    不好适配，

--- hook/build.gradle.kts
+++ hook/build.gradle.kts
@@ -48,7 +48,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
